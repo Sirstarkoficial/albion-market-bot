401fbd0e91b629cca6d1413b54311d144007e8d4
Sirstarkoficial <lentamazacuta123@gmail.com>
2025-12-11 15:26:46 -0800

    Delete utils directory

--- utils/albion_api.py
+++
@@ -1,8 +0,0 @@
-import aiohttp
-
-API_URL = "https://www.albion-online-data.com/api/v2/stats/prices/{}"
-
-async def obtener_precios_item(item_id: str):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(API_URL.format(item_id)) as resp:
-            return await resp.json()
